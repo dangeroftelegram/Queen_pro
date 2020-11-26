8f116827fc2810cd72fda69925aae5cd126081d4
dangeroftelegram <67426127+dangeroftelegram@users.noreply.github.com>
2020-11-26 13:41:43 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -210,7 +210,7 @@ def start(update: Update, context: CallbackContext):
                              url=f"https://t.me/{SUPPORT_CHAT}"),
                          InlineKeyboardButton(
                              text="🔔 Updates Channel",
-                             url="https://t.me/Abhi")
+                             url="https://t.me/Abhi2255o")
                     ], 
             
                      [
