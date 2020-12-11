b5e9d4e857948d5ff5dee684eb31782a1a9dd92f
dangeroftelegram <67426127+dangeroftelegram@users.noreply.github.com>
2020-12-12 02:03:42 +0530

    Update gban.py

--- SaitamaRobot/modules/gban.py
+++ SaitamaRobot/modules/gban.py
@@ -128,4 +128,4 @@ async def type_ban(event):
             to_check = get_reason(id=event.sender_id)
             reason = to_check["reason"]
             bannerid = to_check["bannerid"]
-            await tbot(EditBannedRequest(event.
+            await DRAGONS(EditBannedRequest(event.
